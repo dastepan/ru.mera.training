776f28541fe2913935c553a5be9c5a2cefd01b44
alekulikov <sanek964@gmail.com>
2017-10-13 18:30:12 +0300

    First commit:Changed .java file

--- src/test/java/ru/mera/training/TrainingApplicationTests.java
+++ src/test/java/ru/mera/training/TrainingApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TrainingApplicationTests {
-
+	int var = 12;
 	@Test
 	public void contextLoads() {
 	}
